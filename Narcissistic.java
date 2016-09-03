02726f5c733123f751111ff53e17d8be02938128
Xu Dan <xudaaaaan@163.com>
2016-09-03 13:07:04 +0800

    Add files via upload

---
+++ Narcissistic.java
@@ -0,0 +1,28 @@
+/**
+ * 打印出所有的"水仙花数"，所谓"水仙花数"是指一个三位数，其各位数字立方和等于该数本身。
+ * @author xuyao
+ *
+ */
+public class Narcissistic {
+
+	public static void main(String[] args) {
+		for(int n = 100; n <= 999; n ++){
+			if(isNarcissistic(n)){
+				System.out.println(n);
+			}
+		}
+	}
+	
+	public static boolean isNarcissistic(int n){
+		int hun = n / 100;
+		int ten = (n - hun * 100) / 10;
+		int sin = (n - hun * 100 - ten * 10);
+		if( n == (sin * sin * sin + ten * ten * ten + hun * hun * hun)){
+			return true;
+		}else{
+			return false;
+		}
+			
+	}
+
+}
