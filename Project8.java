df2c7e0550008e7af23284f5e3ddd28289ab4e63
Xu Dan <xudaaaaan@163.com>
2016-09-09 12:49:49 +0800

    Add files via upload

---
+++ Project8.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * 求s=a+aa+aaa+aaaa+aa...a的值，其中a是一个数字。例如2+22+222+2222+22222(此时共有5个数相加)，
+几个数相加有键盘控制。 
+ * @author xudan
+ *
+ */
+
+
+public class Project8 {
+	public static void main(String[] args) {
+		Scanner scan1 = new Scanner(System.in);
+		try{
+			System.out.print("Please enter a: ");
+			int a = scan1.nextInt();
+			System.out.print("Please enter n: ");
+			int n = scan1.nextInt();
+			System.out.println(addFun(a,n));
+		}finally{
+			scan1.close();
+		}	
+	}
+	
+	public static int addFun(int a, int n){
+		int sum = 0;
+		for(int i = n; i > 0 ; i -- ){
+			sum += (int) (a * Math.pow(10,n - i) * i);
+		}
+		return sum;	
+	}
+
+}
