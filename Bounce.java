78c19189d2010c9522fdf6baf03a39753237fba8
Xu Dan <xudaaaaan@163.com>
2016-09-10 17:42:48 +0800

    Update Bounce.java

--- Bounce.java
+++ Bounce.java
@@ -1,24 +1,23 @@
 /**
- * һ���100�׸߶��������£�ÿ����غ�����ԭ�߶ȵ�һ�룻�����£������� ��10�����ʱ����������
-���ף���10�η�����ߣ� 
+ * 一球从100米高度自由落下，每次落地后反跳回原高度的一半；再落下，求它在 第10次落地时，共经过多
+少米？第10次反弹多高？ 
  * @author xudan
- * @param: i:��ʾ��i+1����أ�h��ʾÿ����غ󷴵������ĸ߶ȣ� s������ʮ�����ʱ�ܹ������ľ��롣
+ * @param: i:表示第i+1次落地；h表示每次落地后反弹起来的高度； s代表第十次落地时总共经过的距离。
  */
 
 
 public class Bounce {
-
 	public static void main(String[] args) {
 		double h = 50;
 		double s = 100;
 		for(int i = 1; i < 10; i++){
 			s += h * 2;
 			h /= 2;
-	
 		}
-		System.out.println("��ʮ�η���" + h + "�׸ߡ�");
-		System.out.println("��ʮ����غ��ܹ�������" + s + "�ס�");
-
+		System.out.println("第十次反弹" + h + "米高。");
+		System.out.println("第十次落地后总共经过了" + s + "米。");
 	}
-
 }
+运行结果：
+第十次反弹0.09765625米高。
+第十次落地后总共经过了299.609375米。
