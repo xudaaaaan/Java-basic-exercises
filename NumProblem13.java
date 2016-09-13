0d2b36f9f8a8a483efc6a6f5eb759b49e3b43a65
Xu Dan <xudaaaaan@163.com>
2016-09-13 11:58:15 +0800

    Add files via upload

---
+++ NumProblem13.java
@@ -0,0 +1,27 @@
+/**
+ * 一个整数，它加上100后是一个完全平方数，再加上168又是一个完全平方数，请问该数是多少？ 
+ * @author xuyao
+ *
+ */
+public class NumProblem13 {
+
+	public static void main(String[] args) {
+		for(int i = -100;; i++){
+			if(isPerfectSquare(i + 100)&&isPerfectSquare(i + 168)){
+				System.out.println("The number is " + i);
+			}
+		}
+
+	}
+	
+	public static boolean isPerfectSquare(int n){
+		for(int i = 1; i <= n; i++){
+			if(i * i == n){
+				return true;
+			}
+		}
+		return false;
+		
+	}
+
+}
