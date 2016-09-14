e31e3e1cc14042e5c365e48d931268133b3d4ec2
Xu Dan <xudaaaaan@163.com>
2016-09-14 16:50:10 +0800

    Add files via upload

---
+++ WhichDay.java
@@ -0,0 +1,49 @@
+/**
+ * 输入某年某月某日，判断这一天是这一年的第几天？ 
+ * @author xudan
+ *
+ */
+import java.util.Scanner;
+public class WhichDay {
+
+	public static void main(String[] args) {
+		System.out.println("Please enter year, month, date in order: ");
+		Scanner input = new Scanner(System.in);
+
+		int year = input.nextInt();
+		int month = input.nextInt();
+		int day = input.nextInt();
+		input.close();
+		System.out.println(year + "." + month + "." + day + " is the " + whichDay(year,month,day) + "th day of this year.");
+		
+	}
+	
+	/**
+	 * 判断是否为闰年
+	 * @param y
+	 * @return
+	 */
+	public static boolean isLeapYear(int y){
+		return((y % 4 == 0 && y % 100 != 0)||(y % 400 == 0));
+	}
+	
+	/**
+	 * 判断一个日期是一年中的第几天
+	 * @param d
+	 * @return
+	 */
+	public static int whichDay(int y, int m, int d){
+		int[] months = {31,28,31,30,31,30,31,31,30,31,30,31};
+		if(isLeapYear(y)){
+			months[1] = 29;
+		}
+		int n = 0;
+		for(int i = 0; i < m-1; i++){
+			n += months[i];
+		}
+		return n + d;
+	}
+	
+	
+
+}
